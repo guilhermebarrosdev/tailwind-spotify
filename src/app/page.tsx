d1dc059d2fb89fd0e3f9a1e82820abd0e2ab9fe2
Guilherme Barros <wingui3@gmail.com>
2023-04-27 23:32:15 -0300

    :sparkles: Estrutura base do layout definida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <aside>sidebar</aside>
-      <main>main</main>
-      <footer>footer</footer>
+    <div className='h-screen flex flex-col'>
+      <div className='flex flex-1'>
+        <aside className='w-72 bg-zinc-950 p-6'>sidebar</aside>
+        <main className='flex-1 p-6'>main</main>
+      </div>
+      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
+        footer
+      </footer>
     </div>
   );
 }
